4dd89c30f4e2408dd44559fbb68a2056ec439693
botis_t <boris@talovikov.ru>
2014-08-03 00:39:25 +0600

    code review

--- augmented_banner.php
+++ augmented_banner.php
@@ -1,90 +1,102 @@
 <?php
 
-$config = new Config(KB_SITE);
+class augmented_banner {
 
-function setDefaultConfig($key, $value) {
-  if (config::get($key) == NULL) config::set($key, $value);
-}
+  function add($home) {
 
-function banner_sort_callback($a, $b) {
-  if ($a['cnt'] == $b['cnt']) {
-    return 0;
+    global $smarty;
+
+    self::setDefaultConfig('augmented_banner_numDays', 7);
+    self::setDefaultConfig('augmented_banner_maxDisplayed', 27);
+    self::setDefaultConfig('augmented_banner_displayCorps', 'true');
+    self::setDefaultConfig('augmented_banner_displayPilots', 'true');
+    self::setDefaultConfig('augmented_banner_displayType', 'mixed');
+
+    $numDays = (int) config::get('augmented_banner_numDays');
+    $maxDisplayed = (int) config::get('augmented_banner_maxDisplayed');
+    $displayCorps = 'true' == config::get('augmented_banner_displayCorps');
+    $displayPilots = 'true' == config::get('augmented_banner_displayPilots');
+    $displayType = config::get('augmented_banner_displayType');
+    $alliID = config::get('cfg_allianceid');
+    $corpID = config::get('cfg_corpid');
+    $pilotID = config::get('cfg_pilotid');
+    $podNoobs = config::get('podnoobs');
+
+    $items = array();
+
+    if (!empty($alliID) && $displayCorps) {
+      $corpList = new TopCorpKillsList();
+      $corpList->addInvolvedAlliance($alliID);
+      $corpList->setLimit($maxDisplayed);
+      $corpList->setPodsNoobShips($podNoobs);
+      $corpList->setStartDate(date('Y-m-d H:i',strtotime("- $numDays days")));
+      $corpList->generate();
+      while ($row = $corpList->getRow()) {
+        $corp = new Corporation($row['crp_id']);
+        $items[] = array(
+          'cnt' => $row['cnt'],
+          'obj' => $corp,
+        );
+      }
+    }
+
+    if ($displayPilots) {
+      $list = new TopKillsList();
+      $list->setLimit($maxDisplayed);
+      if (!empty($alliID)) {
+        $list->addInvolvedAlliance($alliID);
+      }
+      if (!empty($corpID)) {
+        $list->addInvolvedCorp($corpID);
+      }
+      if (!empty($pilotID)) {
+        $list->addInvolvedPilot($pilotID);
+      }
+      $list->setPodsNoobShips($podNoobs);
+      $list->setStartDate(date('Y-m-d H:i',strtotime("- $numDays days")));
+      $list->generate();
+      while ($row = $list->getRow() ) {
+        $pilot = new Pilot($row['plt_id']);
+        $items[] = array(
+          'cnt' => $row['cnt'],
+          'obj' => $pilot,
+        );
+      }
+    }
+
+    if(!empty($items)) {
+
+      if ($displayType == 'mixed') {
+        uasort($items, function ($a, $b) {
+            if ($a['cnt'] == $b['cnt']) {
+            return 0;
+          }
+          return ($a['cnt'] > $b['cnt']) ? -1 : 1;
+        });
+      }
+
+      $html = '';
+      $numListed = 0;
+      foreach ($items as $item) {
+        $src = $item['obj']->getPortraitURL(32);
+        $href = $item['obj']->getDetailsURL();
+        $name = $item['obj']->getName();
+        $count = $item['cnt'];
+        $alt = "$name - $count kills over the last $numDays days";
+        $html .= "<div class=\"augmented-banner-item\"><a href=\"${href}\"><img class=\"augmented-banner-img\" src=\"${src}\" alt=\"${alt}\" title=\"${alt}\" /></a></div>";
+        if (++$numListed >= $maxDisplayed) break;
+      }
+
+      $html = "<div class=\"augmented-banner-wrap\">${html}</div>";
+      $smarty->assign('augmented_banner', $html);
+      $home->addHeader('<link rel="stylesheet" type="text/css" href="' . KB_HOST . '/mods/augmented_banner/augmented_banner.css" />');
+
+    }
+
   }
-  return ($a['cnt'] > $b['cnt']) ? -1 : 1;
-}
 
-setDefaultConfig('augmented_banner_numDays', 7);
-setDefaultConfig('augmented_banner_maxDisplayed', 27);
-setDefaultConfig('augmented_banner_displayCorps', 'true');
-setDefaultConfig('augmented_banner_displayPilots', 'true');
-setDefaultConfig('augmented_banner_displayType', 'mixed');
-
-$numDays = (int) config::get('augmented_banner_numDays');
-$maxDisplayed = (int) config::get('augmented_banner_maxDisplayed');
-$displayCorps = 'true' == config::get('augmented_banner_displayCorps');
-$displayPilots = 'true' == config::get('augmented_banner_displayPilots');
-$displayType = config::get('augmented_banner_displayType');
-$alliID = config::get('cfg_allianceid');
-$corpID = config::get('cfg_corpid');
-$pilotID = config::get('cfg_pilotid');
-$podNoobs = config::get('podnoobs');
-
-$items = array();
-
-if (!empty($alliID) && $displayCorps) {
-  $corpList = new TopCorpKillsList();
-  $corpList->addInvolvedAlliance($alliID);
-  $corpList->setLimit($maxDisplayed);
-  $corpList->setPodsNoobShips($podNoobs);
-  $corpList->setStartDate(date('Y-m-d H:i',strtotime("- $numDays days")));
-  $corpList->generate();
-  while ($row = $corpList->getRow()) {
-    $corp = new Corporation($row['crp_id']);
-    $items[] = array(
-      'cnt' => $row['cnt'],
-      'obj' => $corp,
-    );
+  private function setDefaultConfig($key, $value) {
+    if (config::get($key) == NULL) config::set($key, $value);
   }
-}
 
-if ($displayPilots) {
-  $list = new TopKillsList();
-  $list->setLimit($maxDisplayed);
-  if (!empty($alliID)) {
-    $list->addInvolvedAlliance($alliID);
-  }
-  if (!empty($corpID)) {
-    $list->addInvolvedCorp($corpID);
-  }
-  if (!empty($pilotID)) {
-    $list->addInvolvedPilot($pilotID);
-  }
-  $list->setPodsNoobShips($podNoobs);
-  $list->setStartDate(date('Y-m-d H:i',strtotime("- $numDays days")));
-  $list->generate();
-  while ($row = $list->getRow() ) {
-    $pilot = new Pilot($row['plt_id']);
-    $items[] = array(
-      'cnt' => $row['cnt'],
-      'obj' => $pilot,
-    );
-  }
 }
-
-if ($displayType == 'mixed') {
-  uasort($items, "banner_sort_callback");
-}
-
-$html = '';
-$numListed = 0;
-foreach ($items as $item) {
-  $src = $item['obj']->getPortraitURL(32);
-  $href = $item['obj']->getDetailsURL();
-  $name = $item['obj']->getName();
-  $count = $item['cnt'];
-  $alt = "$name - $count kills over the last $numDays days";
-  $html .= "<div class=\"augmented-banner-item\"><a href=\"${href}\"><img class=\"augmented-banner-img\" src=\"${src}\" alt=\"${alt}\" title=\"${alt}\" /></a></div>";
-  if (++$numListed >= $maxDisplayed) break;
-}
-
-$html = "<div class=\"augmented-banner-wrap\">${html}</div>";
--- init.php
+++ init.php
@@ -1,19 +1,12 @@
 <?php
 
-$modInfo['augmented_banner']['name'] = "Augmented Banner";
+require_once('mods/augmented_banner/augmented_banner.php');
+
+$modInfo['augmented_banner']['name'] = 'Augmented Banner';
 $modInfo['augmented_banner']['abstract'] = "If you have enabled this mod and don't see corps or pilots beneath your banner, then you have probably not added this line to your active template's <strong>index.tpl</strong> file:<br /><br />
 <code>{if isset(\$augmented_banner)}{\$augmented_banner}{/if}</code><br /><br />
 Look for the first table with the class navigation and add that line prior to the table line.";
 $modInfo['augmented_banner']['about'] = 'Created by Squizz Caphinator, patched by Boris Blade Artrald.<br />
 <a href="https://github.com/6RUN0/augmented_banner">Get Augmented Banner</a>';
 
-event::register("page_assembleheader", "augmented_banner::add");
-
-class augmented_banner {
-  function add($home){
-    global $smarty;
-    include_once('mods/augmented_banner/augmented_banner.php');
-    $smarty->assign("augmented_banner", $html);
-    $home->addHeader('<link rel="stylesheet" type="text/css" href="' . config::get('cfg_kbhost') . '/mods/augmented_banner/augmented_banner.css" />');
-  }
-}
+event::register('page_assembleheader', 'augmented_banner::add');
